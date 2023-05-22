7da3bc9d918666b2ef5ed1d557402dc3e1ec01c0
keedem <liugoldent@gmail.com>
2023-05-22 12:03:52 +0800

    fix template

--- pages/capitalCode.js
+++ pages/capitalCode.js
@@ -1,8 +1,41 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, useDeferredValue } from "react";
+console.log(process.env.API_BASE_URL)
 export default function CapitalCode() {
   const [bullCode, setBullCode] = useState('');
   const [bearCode, setBearCode] = useState('');
+  const [dbBullCode, setDbBullCode] = useState([])
+  const [dbBearCode, setDbBearCode] = useState([])
+  useEffect(() => {
+    // 在組件剛開始渲染時執行的函式
+    const initFunc = async () => {
+      setDbBullCode(await fetchCodeList('bull'))
+      setDbBearCode(await fetchCodeList('bear'))
+    }
+    initFunc()
+    // fetchCodeList('bull')
+    //   fetchCodeList('bear')
+    // 清除副作用函式
+    return () => {
+      // 在組件卸載時執行的清除函式
+    };
+  }, []); // 第二個參數為空陣列，只在剛開始渲染時執行一次
+  const fetchCodeList = async (type) => {
+    const result = await fetch(`${process.env.API_BASE_URL}/stock/getBullBearCode`, {
+      method: 'POST',
+      body: JSON.stringify({
+        collectionName: type
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.text()) // 將 Response 轉換為文字格式
+      .then(text => {
+        return text
+      }) // 在控制台中顯示文字資料
+    return result
+  }
+
   const handleBullInputChange = (e) => {
     setBullCode(e.target.value)
   }
@@ -32,10 +65,12 @@ export default function CapitalCode() {
           'Content-Type': 'application/json'
         }
       })
-      if(result.status === 200){
+      if (result.status === 200) {
         alert('修改成功')
         setBullCode('')
         setBearCode('')
+        setDbBullCode(await fetchCodeList('bull'))
+        setDbBearCode(await fetchCodeList('bear'))
       }
     } catch (error) {
       console.error(error.message)
@@ -83,11 +118,12 @@ export default function CapitalCode() {
         </div>
         <button onClick={() => updateCode('bull')} className="group self-end	rounded-2xl mt-2 h-10 lg:w-1/5 sm:w-full md:w-full bg-red-500 font-bold text-lg text-white relative overflow-hidden">
           更新多方!
-          <div className="absolute duration-300 inset-0 w-full h-full transition-all scale-0 group-hover:scale-100 group-hover:bg-white/30 rounded-2xl"></div>
+          <div className="absolute duration-300 inset-0 w-full h-full transition-all scale-0 rounded-2xl"></div>
         </button>
+        <p>目前多方資料庫代碼：{dbBullCode}</p>
 
         {/* 更新空方 */}
-        <div className="hidden relative flex h-10 w-full flex-row-reverse overflow-clip rounded-lg mt-4">
+        <div className="relative flex h-10 w-full flex-row-reverse overflow-clip rounded-lg mt-4">
           <input
             value={bearCode}
             onChange={handleBearInputChange}
@@ -104,12 +140,12 @@ export default function CapitalCode() {
             空
           </label>
         </div>
-        <button onClick={() => updateCode('bear')} className="hidden group self-end mt-2 rounded-2xl h-10 lg:w-1/5 sm:w-full md:w-full bg-green-500 font-bold text-lg text-white overflow-hidden">
+        <button onClick={() => updateCode('bear')} className="group self-end mt-2 rounded-2xl h-10 lg:w-1/5 sm:w-full md:w-full bg-green-500 font-bold text-lg text-white overflow-hidden">
           更新空方!
-          <div className="absolute duration-300 inset-0 w-full h-full transition-all scale-0 group-hover:scale-100 group-hover:bg-white/30 rounded-2xl"></div>
+          <div className="absolute duration-300 inset-0 w-full h-full transition-all scale-0 rounded-2xl"></div>
         </button>
-
-        <div className="mt-4 flex flex-col justify-end items-end">
+        <p>目前空方資料庫代碼：{dbBearCode}</p>
+        {/* <div className="mt-4 flex flex-col justify-end items-end">
           <button onClick={() => startFunc()} className="group rounded-2xl mb-2 h-12 w-48 bg-blue-500 font-bold text-lg text-white relative overflow-hidden">
             監測開始
             <div className="absolute duration-300 inset-0 w-full h-full transition-all scale-0 group-hover:scale-100 group-hover:bg-white/30 rounded-2xl"></div>
@@ -118,7 +154,7 @@ export default function CapitalCode() {
             監測結束
             <div className="absolute duration-300 inset-0 w-full h-full transition-all scale-0 group-hover:scale-100 group-hover:bg-white/30 rounded-2xl"></div>
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
